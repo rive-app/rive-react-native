2d249d24c890ae6ace2393f0eace95d758a2af00
Tomasz Krzyżowski <tomasz.krzyzowski@callstack.com>
2021-05-19 09:05:01 +0200

    feat: added multiple animations example (#25)
    * feat: added multiple animations example
    
    * fix: set proper active buttons once reset button has been pressed

--- example/src/Home.tsx
+++ example/src/Home.tsx
@@ -14,6 +14,15 @@ export default function Home({ navigation }) {
         >
           Simple
         </Button>
+
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate('MultipleAnimations')}
+          style={styles.buttonStyle}
+        >
+          Multiple animations
+        </Button>
+
         <Button
           mode="contained"
           onPress={() => navigation.navigate('Http')}
