e267efb2248221168d13c11cde84883c3d4af599
TMaszko <tomasz.krzyzowski@callstack.com>
2021-05-20 09:40:31 +0200

    feat: updated pod source and added example file

--- ios/MyRiveView.swift
+++ ios/MyRiveView.swift
@@ -2,20 +2,24 @@ import UIKit
 import RiveRuntime
 
 class MyRiveView: UIView {
-    var artboard: RiveArtboard?;
-
-    func updateArtboard(_ artboard: RiveArtboard) {
-        self.artboard = artboard;
+    var riveView: RiveView!
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        riveView = RiveView()
     }
 
-    override func draw(_ rect: CGRect) {
-        guard let context = UIGraphicsGetCurrentContext(), let artboard = self.artboard else {
-            return
-        }
-        let renderer = RiveRenderer(context: context);
-        renderer.align(with: rect, withContentRect: artboard.bounds(), with: Alignment.Center, with: Fit.Contain)
-        artboard.draw(renderer)
-    }
+//    func updateArtboard(_ artboard: RiveArtboard) {
+//        self.artboard = artboard;
+//    }
+
+//    override func draw(_ rect: CGRect) {
+//        guard let context = UIGraphicsGetCurrentContext(), let artboard = self.artboard else {
+//            return
+//        }
+//        let renderer = RiveRenderer(context: context);
+//        renderer.align(with: rect, withContentRect: artboard.bounds(), with: Alignment.Center, with: Fit.Contain)
+//        artboard.draw(renderer)
+//    }
 
 }
 
--- ios/RiveReactNativeViewManager.m
+++ ios/RiveReactNativeViewManager.m
@@ -1,5 +1,5 @@
 #import "React/RCTBridgeModule.h"
-#import "RCTViewManager.h"
+#import "React/RCTViewManager.h"
 
 @interface RCT_EXTERN_MODULE(RiveReactNativeViewManager, RCTViewManager)
 
