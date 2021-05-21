37c85531d87dd8f9ed2c663815f583e3c28f38f7
michal.muzyk@callstack.com <michal.muzyk@callstack.com>
2021-05-21 13:00:46 +0200

    feat: onPause for ios

--- ios/RiveReactNativeViewManager.m
+++ ios/RiveReactNativeViewManager.m
@@ -8,6 +8,7 @@ RCT_EXPORT_VIEW_PROPERTY(url, NSString)
 RCT_EXPORT_VIEW_PROPERTY(fit, NSString)
 RCT_EXPORT_VIEW_PROPERTY(alignment, NSString)
 RCT_EXPORT_VIEW_PROPERTY(onPlay, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onPause, RCTDirectEventBlock)
 
 RCT_EXTERN_METHOD(play:(nonnull NSNumber *)node animationNames:(nonnull NSArray *)animationNames loopMode:(NSString)loopMode direction:(NSString)direction areStateMachines:(BOOL)areStateMachines)
 RCT_EXTERN_METHOD(pause:(nonnull NSNumber *)node animationNames:(nonnull NSArray *)animationNames areStateMachines:(BOOL)areStateMachines)
