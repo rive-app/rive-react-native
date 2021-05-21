35bbcbf072ff532f911e2eca9de2af5bd98d2b27
TMaszko <tomasz.krzyzowski@callstack.com>
2021-05-21 07:59:24 +0200

    feat: added url props and prepared initial reloading architecture

--- ios/RiveReactNativeViewManager.m
+++ ios/RiveReactNativeViewManager.m
@@ -4,6 +4,7 @@
 @interface RCT_EXTERN_MODULE(RiveReactNativeViewManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(resourceName, NSString)
+RCT_EXPORT_VIEW_PROPERTY(url, NSString)
 RCT_EXPORT_VIEW_PROPERTY(fit, NSString)
 
 RCT_EXTERN_METHOD(play:(nonnull NSNumber *)node animationNames:(nonnull NSArray *)animationNames loopMode:(NSString)loopMode direction:(NSString)direction areStateMachines:(BOOL)areStateMachines)
