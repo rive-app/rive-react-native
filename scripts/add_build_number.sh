61b14a0eb789d944ce6a4c34e5111807e9ecab6e
Zach Plata <plata.zach@gmail.com>
2022-11-22 10:04:44 -0600

    fix: fix version bump publish workflow on GH actions to manually publish version from package.json

--- scripts/add_build_number.sh
+++ scripts/add_build_number.sh
@@ -4,6 +4,5 @@ get_package_version() {
 	PACKAGE_VERSION="$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)"
 }
 get_package_version
-PACKAGE_WITHOUT_PATCH=$(echo $PACKAGE_VERSION | sed "s/\./\n/g" | head -2 | sed "N;s/\n/\./g")
 
-echo "$PACKAGE_WITHOUT_PATCH.$1"
+echo "$PACKAGE_VERSION"
