eeaab751cf59c22ec47f9959df9d3b0305407775
Lance <lance@lancesnider.com>
2024-10-15 15:16:29 -0700

    chore: upgrade rive-ios runtime to 5.16

--- ios/RiveReactNative-Bridging-Header.h
+++ ios/RiveReactNative-Bridging-Header.h
@@ -1,7 +1,7 @@
 #import <RiveRuntime/RiveRuntime.h>
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
-#import "React/RCTUIManager.h"
+#import <React/RCTUIManager.h>
 #import <React/RCTUtils.h>
-#import "React/RCTExceptionsManager.h"
+#import <React/RCTExceptionsManager.h>
 #import <React/RCTView.h>
