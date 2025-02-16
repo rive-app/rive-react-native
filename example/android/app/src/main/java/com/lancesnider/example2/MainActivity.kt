65b72c20a76f53c067f92cfe9026bd5dc4ef9553
Lance <lance@lancesnider.com>
2025-02-15 18:48:40 -0800

    chore: rename examples-2 to examples

--- example/android/app/src/main/java/com/lancesnider/example2/MainActivity.kt
+++ example/android/app/src/main/java/com/lancesnider/example2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lancesnider.example2
+package com.lancesnider.example
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- example/android/app/src/main/java/com/lancesnider/example2/MainApplication.kt
+++ example/android/app/src/main/java/com/lancesnider/example2/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.lancesnider.example2
+package com.lancesnider.example
 
 import android.app.Application
 import android.content.res.Configuration
