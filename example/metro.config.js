2b31c53b9938b45229fbf0522505272533ca735b
CI <pggordonhayes@gmail.com>
2025-02-19 19:33:12 +0100

    feat: replace old example with latest expo

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,40 +1,30 @@
+const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/exclusionList');
-const escape = require('escape-string-regexp');
-const pak = require('../package.json');
 
-const root = path.resolve(__dirname, '..');
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const monorepoRoot = path.resolve(projectRoot, '../');
 
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
+const config = getDefaultConfig(projectRoot);
 
-module.exports = {
-  projectRoot: __dirname,
-  watchFolders: [root],
+// 1. Watch all files within the monorepo
+config.watchFolders = [monorepoRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
 
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
-  resolver: {
-    blacklistRE: blacklist(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
+module.exports = config;
 
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
+// const path = require('path');
 
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+// module.exports = {
+//   resolver: {
+//     extraNodeModules: {
+//       'rive-react-native': path.resolve(__dirname, '..'),
+//     },
+//   },
+//   watchFolders: [path.resolve(__dirname, '..')],
+// };
