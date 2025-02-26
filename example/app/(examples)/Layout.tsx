21378cbea5808daf5513b85a6da3265d20f8bb1e
Lance <lance@lancesnider.com>
2025-02-25 19:29:35 -0800

    Get everything working i Android

--- example/app/(examples)/Layout.tsx
+++ example/app/(examples)/Layout.tsx
@@ -5,6 +5,7 @@ import { isEnumKey } from '../../constants/typesPredicates';
 import { Picker } from '@react-native-picker/picker';
 
 const resourceName = 'truck_v7';
+const artboardName = 'New Artboard';
 
 export default function Layout() {
   const [fit, setFit] = React.useState(Fit.Cover);
